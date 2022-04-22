6e50b99037fe13d9fde5d8a8759a00e62e1e3dd4
Vrushank Agrawal <vrushank.agrawal@thon.polytechnique.fr>
2022-04-22 14:04:16 +0200

    cards data in json

--- card_data/ml.py
+++ card_data/ml.py
@@ -1,12 +1,12 @@
 import urllib.request
 import json
 
-with open("mlkey.txt") as f1:
+with open("api_key_thon.txt") as f1:
     api = f1.read().rstrip('\n')
-    base_url = "https://api.clashroyale.com/v1"
-    endpoint = "/cards"
+    url = "https://api.clashroyale.com/v1"
+    endobject = "/cards"
     request = urllib.request.Request(
-                    base_url+endpoint,
+                    url+endobject,
                     None,
                     {
                         "Authorization": "Bearer %s" %api
@@ -14,4 +14,3 @@ with open("mlkey.txt") as f1:
             )
     response = urllib.request.urlopen(request).read().decode("utf-8")
     print(response)
-
