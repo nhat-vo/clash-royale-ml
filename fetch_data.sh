d594423655381885a969a92e21ec2130882f43e9
Nhat Vo <tonystark0243@gmail.com>
2022-04-25 22:25:22 +0200

    added variables and progress bar to fetch script

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,23 +4,40 @@
 API_KEY=$(cat apikey.txt)
 
 # params (check https://developer.clashroyale.com/#/documentation for more details)
-LOCATION=57000255
-PLAYER_LIMIT=100
+LOCATION=${2:-57000255}
+PLAYER_LIMIT=${1:-100}
 
 # download cards and stats
 curl https://royaleapi.github.io/cr-api-data/json/cards.json > cards.json
 curl https://royaleapi.github.io/cr-api-data/json/cards_stats.json > cards_stats.json
 
 # download top $PLAYER_LIMIT players
-curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/locations/$LOCATION/rankings/players?limit=$PLAYER_LIMIT" | tr '"' '\n' | grep '#' > players.txt
+curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/locations/$LOCATION/rankings/players?limit=$PLAYER_LIMIT" > players.json
+cat players.json | tr ',' '\n' | grep -v 'clan' | tr '"' '\n' | grep '#' > players.txt
 
 # download battle logs from top players
 readarray -t players < players.txt
 rm -rf battles
 mkdir battles
 
+PLAYER_LIMIT=${#players[@]}
+
+i=1
 for player in "${players[@]}"
 do
+	let tmp=i*100/$PLAYER_LIMIT
+	let prog=tmp/5
+	let i++
+	printf "Downloading: ["
+	for ((j=0; j<$prog; j++))
+	do
+		printf '='
+	done
+	for ((j=$prog; j < 20; j++)) 
+	do
+		printf '-'
+	done
+	printf "] ($tmp%%) \r"
 	FILENAME=$(echo $player | cut -d '#' -f 2)
 	curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/players/%23$FILENAME/battlelog" > "./battles/$FILENAME.json" 2> /dev/null
 
@@ -30,6 +47,7 @@ do
 		rm -f "./battles/$FILENAME.json"
 	fi
 done
+printf '\n'
 
 # count how many players were downloaded
 echo "$(ls battles | wc -l) players downloaded"
