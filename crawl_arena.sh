294ae1e3faac01cabdd8ec89b9c981db10767f50
Nhat Vo <tonystark0243@gmail.com>
2022-04-27 11:15:50 +0200

    added crawl by list of player

--- crawl_arena.sh
+++ crawl_arena.sh
@@ -4,10 +4,24 @@
 API_KEY=$(cat apikey.txt)
 
 PLAYER=$1
-PLAYER_LIMIT=${2:-100}
 
-curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/players/%23$PLAYER/battlelog" > "players.json" 2> /dev/null
-cat players.json | tr ',' '\n' | grep 'opponent' | tr '"' '\n' | grep '#' > players.txt
+readarray -t players < ./start_crawl.txt
+
+PLAYER_LIMIT=${#players[@]}
+
+echo "Fetching players"
+i=1
+for player in "${players[@]}"
+do
+	let i++
+	FILENAME=$(echo $player | cut -d '#' -f 2)
+	curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/players/%23$FILENAME/battlelog" > players.json 2> /dev/null
+	cat players.json | tr ',' '\n' | grep 'opponent' | tr '"' '\n' | grep '#' >> players.txt
+done
+printf '\n'
+
+# count how many players were downloaded
+echo "$(ls ./battles | wc -l) players downloaded"
 
 ./fetch_scripts/fetch_cards.sh
 ./fetch_scripts/fetch_matches.sh
--- crawl_top.sh
+++ crawl_top.sh
@@ -7,9 +7,11 @@ API_KEY=$(cat apikey.txt)
 LOCATION=${2:-57000255}
 PLAYER_LIMIT=${1:-100}
 
+
 # download top $PLAYER_LIMIT players
 curl -H "Authorization: Bearer $API_KEY" "https://api.clashroyale.com/v1/locations/$LOCATION/rankings/players?limit=$PLAYER_LIMIT" > players.json
-cat players.json | tr ',' '\n' | grep -v 'clan' | tr '"' '\n' | grep '#' > players.txt
+cat players.json | tr ',' '\n' | grep -v 'clan' | tr '"' '\n' | grep '#' >> players.txt
+
 
 ./fetch_scripts/fetch_cards.sh
 ./fetch_scripts/fetch_matches.sh
